b7b57042f9ab56999fceb26904f435978f289801
FeanorD <meleshkodim2000@gmail.com>
2020-01-28 22:14:38 +0200

    add Term class

---
+++ FL12_HW12/homework/src/app.js
@@ -0,0 +1,19 @@
+const ADD_SET_PAGE = '#/add';
+const MODIFY_PAGE = '#/modify';
+
+const setList = document.querySelector('.set-list');
+
+class TermSet {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Term {
+    constructor(name, definition) {
+        this.name = name;
+        this.definition = definition;
+    }
+
+
+}
